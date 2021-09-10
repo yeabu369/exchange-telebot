b641b625bc791eaf0d7473ba623b0d045d9fbce9
yeabu369 <yeabsdebe@gmail.com>
2021-09-10 18:11:08 +0000

    boilerplate setup

---
+++ app.js
@@ -0,0 +1,103 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf('1353688196:AAFdoPz4zETGB2kega04LkVFFoPz0fOSxmw');
+
+//method for invoking start command
+ 
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Hello! I am BTC-ETB Order Bot. What is your name?', {
+    })
+});
+
+//method that displays the inline keyboard buttons 
+
+bot.hears('animals', ctx => {
+    console.log(ctx.from)
+    let animalMessage = `great, here are pictures of animals you would love`;
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                        text: "dog",
+                        callback_data: 'dog'
+                    },
+                    {
+                        text: "cat",
+                        callback_data: 'cat'
+                    }
+                ],
+
+            ]
+        }
+    })
+});
+
+//method that returns image of a dog
+
+bot.action('dog', ctx => {
+    bot.telegram.sendPhoto(ctx.chat.id, {
+        source: "res/dog.jpeg"
+    })
+
+});
+
+//method that returns image of a cat 
+
+bot.action('cat', ctx => {
+    bot.telegram.sendPhoto(ctx.chat.id, {
+        source: "res/cat.jpeg"
+    })
+
+});
+
+//method for requesting user's phone number
+
+bot.hears('phone', (ctx, next) => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Can we get access to your phone number?', requestPhoneKeyboard);
+
+});
+
+//method for requesting user's location
+
+bot.hears("location", (ctx) => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Can we access your location?', requestLocationKeyboard);
+});
+
+//constructor for providing phone number to the bot
+
+const requestPhoneKeyboard = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [
+            [{
+                text: "My phone number",
+                request_contact: true,
+                one_time_keyboard: true
+            }],
+            ["Cancel"]
+        ]
+    }
+};
+//constructor for proving location to the bot
+
+const requestLocationKeyboard = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [
+            [{
+                text: "My location",
+                request_location: true,
+                one_time_keyboard: true
+            }],
+            ["Cancel"]
+        ]
+    }
+
+}
+
+
+bot.launch();
