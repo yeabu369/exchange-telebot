88e1e54e2c539a7e4498a3de849b66a8cd5e58d7
yeabu369 <yeabsdebe@gmail.com>
2021-09-10 18:34:12 +0000

    Changed to proces.env for token

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const { Telegraf } = require('telegraf');
+require('dotenv').config();
 
-const bot = new Telegraf('1353688196:AAFdoPz4zETGB2kega04LkVFFoPz0fOSxmw');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 //method for invoking start command
- 
+
 bot.command('start', ctx => {
     console.log(ctx.from)
     bot.telegram.sendMessage(ctx.chat.id, 'Hello! I am BTC-ETB Order Bot. What is your name?', {
