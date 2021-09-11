a11e50f3af540af063abd1ed4344b454fb6d6ed7
yeabu369 <yeabsdebe@gmail.com>
2021-09-11 19:23:42 +0000

    Added start and help commands

--- app.js
+++ app.js
@@ -1,104 +1,25 @@
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-//method for invoking start command
-
-bot.command('start', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Hello! I am BTC-ETB Order Bot. What is your name?', {
-    })
+bot.start((ctx) => {
+  ctx.reply(
+    "Hello, I am Rasp-Face-Cog-Bot. You can send me pictures for face recognition."
+  );
 });
 
-//method that displays the inline keyboard buttons 
+const helpMessage = `Here are the list of commands:
+/start - Starts the bot
+/help - Brings up the help text
+/addphotos - Allows user to send 5 photos for face recognition`;
 
-bot.hears('animals', ctx => {
-    console.log(ctx.from)
-    let animalMessage = `great, here are pictures of animals you would love`;
-    ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                        text: "dog",
-                        callback_data: 'dog'
-                    },
-                    {
-                        text: "cat",
-                        callback_data: 'cat'
-                    }
-                ],
-
-            ]
-        }
-    })
+bot.help((ctx) => {
+  ctx.reply(helpMessage);
 });
 
-//method that returns image of a dog
-
-bot.action('dog', ctx => {
-    bot.telegram.sendPhoto(ctx.chat.id, {
-        source: "res/dog.jpeg"
-    })
-
+bot.command(['addphotos', 'AddPhotos', 'addPhotos'], (ctx, next) => {
+    ctx.reply('You can start sending 5 photos of your face now!');
 });
-
-//method that returns image of a cat 
-
-bot.action('cat', ctx => {
-    bot.telegram.sendPhoto(ctx.chat.id, {
-        source: "res/cat.jpeg"
-    })
-
-});
-
-//method for requesting user's phone number
-
-bot.hears('phone', (ctx, next) => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Can we get access to your phone number?', requestPhoneKeyboard);
-
-});
-
-//method for requesting user's location
-
-bot.hears("location", (ctx) => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Can we access your location?', requestLocationKeyboard);
-});
-
-//constructor for providing phone number to the bot
-
-const requestPhoneKeyboard = {
-    "reply_markup": {
-        "one_time_keyboard": true,
-        "keyboard": [
-            [{
-                text: "My phone number",
-                request_contact: true,
-                one_time_keyboard: true
-            }],
-            ["Cancel"]
-        ]
-    }
-};
-//constructor for proving location to the bot
-
-const requestLocationKeyboard = {
-    "reply_markup": {
-        "one_time_keyboard": true,
-        "keyboard": [
-            [{
-                text: "My location",
-                request_location: true,
-                one_time_keyboard: true
-            }],
-            ["Cancel"]
-        ]
-    }
-
-}
-
 
 bot.launch();
