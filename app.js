a5f836b1bd506c0d25ec8892026682b44cacb89b
yeabu369 <yeabsdebe@gmail.com>
2021-09-22 09:55:35 +0000

    Testing scenes

--- app.js
+++ app.js
@@ -1,95 +1,37 @@
-const Telegraf = require("telegraf");
-const Stage = require("telegraf/stage");
-const session = require("telegraf/session");
-const Scene = require("telegraf/scenes/base");
-const { leave } = Stage;
-const stage = new Stage();
-
-require("dotenv").config();
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-const getName = new Scene("getName");
-stage.register(getName);
-const getPhotos = new Scene("getPhotos");
-stage.register(getPhotos);
-
-bot.use(session());
+"use strict";
+exports.__esModule = true;
+/* eslint-disable @typescript-eslint/no-floating-promises */
+var telegraf_1 = require("telegraf");
+require('dotenv').config();
+var token = process.env.BOT_TOKEN;
+if (token === undefined) {
+    throw new Error('BOT_TOKEN must be provided!');
+}
+// Handler factories
+var _a = telegraf_1.Scenes.Stage, enter = _a.enter, leave = _a.leave;
+// Greeter scene
+var greeterScene = new telegraf_1.Scenes.BaseScene('greeter');
+greeterScene.enter(function (ctx) { return ctx.reply('Hi'); });
+greeterScene.leave(function (ctx) { return ctx.reply('Bye'); });
+greeterScene.hears('hi', enter('greeter'));
+greeterScene.on('message', function (ctx) { return ctx.replyWithMarkdown('Send `hi`'); });
+// Echo scene
+var echoScene = new telegraf_1.Scenes.BaseScene('echo');
+echoScene.enter(function (ctx) { return ctx.reply('echo scene'); });
+echoScene.leave(function (ctx) { return ctx.reply('exiting echo scene'); });
+echoScene.command('back', leave());
+echoScene.on('text', function (ctx) { return ctx.reply(ctx.message.text); });
+echoScene.on('message', function (ctx) { return ctx.reply('Only text messages please'); });
+var bot = new telegraf_1.Telegraf(token);
+var stage = new telegraf_1.Scenes.Stage([greeterScene, echoScene], {
+    ttl: 10
+});
+bot.use((0, telegraf_1.session)());
 bot.use(stage.middleware());
-
-user = {
-  isRegistered: true,
-};
-
-const helpMessage = `Here are the list of commands:
-/start - Starts the bot
-/help - Brings up the help text
-/addphotos - Allows user to send 5 photos for face recognition
-/Register - Registers a first time user`;
-
-bot.start((ctx) => {
-  ctx.reply(
-    "Hello, I am Rasp-Face-Cog-Bot. You can send me pictures for face recognition. Enter your first name and last name.",
-    { reply_markup: { remove_keyboard: true } }
-  );
-  ctx.scene.enter("getName");
-});
-
-bot.help((ctx) => {
-  ctx.reply(helpMessage);
-});
-
-getName.on("text", async (ctx) => {
-  if (ctx.message.text === "◀️ Back") {
-    ctx.reply(
-      "You are already back at the very beginning. Please enter your name."
-    );
-    ctx.scene.enter("getName");
-  } else {
-    ctx.session.name = ctx.message.text;
-    ctx.reply(
-      "Next Upload 5 photos" +
-        `\n\nAlready entered data:\nFULL NAME: ${ctx.session.name}`,
-      {
-        reply_markup: {
-          keyboard: [["◀️ Back"]],
-          resize_keyboard: true,
-          one_time_keyboard: true,
-        },
-      }
-    );
-
-    await ctx.scene.leave("getName");
-    ctx.scene.enter("getPhotos");
-  }
-});
-
-getPhotos.on("text", (ctx) => {
-  if (ctx.message.text === "◀️ Back") {
-    return ctx.reply(
-      "You are already back at the very beginning. Please enter your name."
-    );
-  }
-});
-
-getPhotos.on("photo", (ctx, next) => {
-  user.isRegistered
-    ? ctx.reply("Thanks, but you have already been registered!")
-    : next(ctx);
-});
-
-bot.command(["addphotos", "AddPhotos", "addPhotos"], (ctx, next) => {
-  ctx.reply("You can start sending 5 photos of your face now!");
-});
-
-bot.on("photo", (ctx, next) => {
-  user.isRegistered
-    ? ctx.reply("Thanks, but you have already been registered!")
-    : next(ctx);
-});
-
-bot.command("register", (ctx) => {
-  ctx.reply("Please enter your fullname below ...");
-});
-
-bot.startPolling();
+bot.command('greeter', function (ctx) { return ctx.scene.enter('greeter'); });
+bot.command('echo', function (ctx) { return ctx.scene.enter('echo'); });
+bot.on('message', function (ctx) { return ctx.reply('Try /echo or /greeter'); });
+bot.launch();
+// Enable graceful stop
+process.once('SIGINT', function () { return bot.stop('SIGINT'); });
+process.once('SIGTERM', function () { return bot.stop('SIGTERM'); });
